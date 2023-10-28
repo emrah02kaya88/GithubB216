14cc8b21c4ef61e99e621afcc474dc9a46fec181
Emrah Kaya <emrah02kaya88@gmail.com>
2023-10-28 22:31:44 +0300

    v2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,6 +2,8 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("versiyon 1 kodları");
 
+        System.out.println("versiyon 2 kodları");
+
 
 
     }
